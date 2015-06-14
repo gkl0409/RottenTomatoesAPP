2436273fc06618569c8b4513b4fb96b5c227f3f4
kaden <kaden@yahoo-inc.com>
2015-06-14 17:42:33 +0800

    Initial Commit

---
+++ RottenTomatoesAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoesAPP
+//
+//  Created by kaden Chiang on 2015/6/14.
+//  Copyright (c) 2015年 kaden Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenTomatoesAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RottenTomatoesAPP
+//
+//  Created by kaden Chiang on 2015/6/14.
+//  Copyright (c) 2015年 kaden Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
