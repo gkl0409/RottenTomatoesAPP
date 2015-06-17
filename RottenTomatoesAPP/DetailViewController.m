9aa770e77efe3592eb2379cf50d1d5e73fdb9dad
kaden <kaden@yahoo-inc.com>
2015-06-17 19:15:40 +0800

    detail add scrollview

--- RottenTomatoesAPP/DetailViewController.m
+++ RottenTomatoesAPP/DetailViewController.m
@@ -12,8 +12,10 @@
 @interface DetailViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *posterImage;
+@property (weak, nonatomic) IBOutlet UIView *detailView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @end
 
 @implementation DetailViewController
@@ -28,6 +30,35 @@
     
     NSString *posterUrlString = [RottenTomatoesUtil hiResImageUrl:[self.movieData valueForKeyPath:@"posters.thumbnail"]];
     [self.posterImage setImageWithURL:[NSURL URLWithString:posterUrlString] placeholderImage: self.placeholderImage];
+    CGRect frame = [[UIScreen mainScreen] bounds];
+
+    
+    CGRect synopsisLabelRect = [self.synopsisLabel.text boundingRectWithSize:CGSizeMake(frame.size.width-20, CGFLOAT_MAX)
+                                                                     options:NSStringDrawingUsesLineFragmentOrigin
+                                                                  attributes:@{NSFontAttributeName: self.synopsisLabel.font}
+                                                                     context:nil];
+
+    CGPoint detailViewOrigin = self.detailView.frame.origin;
+    CGSize detailViewSize = CGSizeMake(frame.size.width, self.titleLabel.frame.size.height + synopsisLabelRect.size.height + 20);
+    if (detailViewSize.height < frame.size.height - self.detailView.frame.origin.y) {
+        detailViewSize.height = frame.size.height - self.detailView.frame.origin.y;
+    }
+    [self.detailView setFrame: CGRectMake(detailViewOrigin.x, detailViewOrigin.y, detailViewSize.width, detailViewSize.height)];
+    NSLog(@"%@",NSStringFromCGRect(self.detailView.frame));
+    
+    synopsisLabelRect.origin.x = 10;
+    synopsisLabelRect.origin.y = self.titleLabel.frame.origin.y + self.titleLabel.frame.size.height + 10;
+    self.synopsisLabel.frame = synopsisLabelRect;
+
+    NSLog(@"%@",NSStringFromCGRect(synopsisLabelRect));
+    NSLog(@"synopsisLabel.frame: %@",NSStringFromCGRect(self.synopsisLabel.frame));
+    NSLog(@"scrollView.bounds: %@",NSStringFromCGRect(self.scrollView.bounds));
+    NSLog(@"synopsisLabel.bounds %@",NSStringFromCGRect(self.synopsisLabel.bounds));
+    NSLog(@"detailView.frame: %@",NSStringFromCGRect(self.detailView.frame));
+
+    [self.scrollView setScrollEnabled:YES];
+    [self.scrollView setContentSize: CGSizeMake(frame.size.width, self.detailView.frame.origin.y + self.detailView.frame.size.height)];
+    
 }
 
 - (void)didReceiveMemoryWarning {
